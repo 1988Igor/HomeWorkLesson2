b40be32c787c1a0ad2c81369d2e13193f2c12a75
Igor <romanigor.conect@gmail.com>
2022-08-22 23:33:52 +0200

    show  coordinates

---
+++ Task3_Range_Coord/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter a number of quarter (1 to 4) ");
+int a  = int.Parse(Console.ReadLine());
+switch(a)
+{
+    case 1:
+        Console.WriteLine("x>0, y>0");
+    break;
+
+    case 2 :
+         Console.WriteLine("x<0, y>0");
+    break;
+
+    case 3:
+        Console.WriteLine("x<0, y<0");
+    break;
+
+    case 4:
+        Console.WriteLine("x>0, y<0");
+    break;
+
+    default:
+        Console.WriteLine("This number is wrong, please enter a number of quarter (1 to 4) !!!");
+    break;
+}
