eb1cbbe702b136644c5491d3b1628d1073e45c7c
Igor <romanigor.conect@gmail.com>
2022-08-22 21:06:10 +0200

    square

---
+++ Task2 _number_power/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the first number: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the second number: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(a =  Math.Pow(a, 2));
+Console.WriteLine(b =  Math.Pow(b, 2));
+
+
+if (a==b)
+{
+    Console.WriteLine("The number is the square of the other ");
+
+}
+
+else
+{
+    Console.WriteLine("Neither number is the square of the other");
+
+}
