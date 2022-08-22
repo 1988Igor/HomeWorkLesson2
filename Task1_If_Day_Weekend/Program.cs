827134b25c6d75ec9ae7d59114469c748c480861
Igor <romanigor.conect@gmail.com>
2022-08-22 20:27:54 +0200

    Task 1 day Weekend

---
+++ Task1_If_Day_Weekend/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter the Day of the Week (1 to 7) ");
+int a = int.Parse(Console.ReadLine());
+
+switch(a)
+{
+        case 1: case 2: case 3: case 4: case 5: 
+
+    Console.WriteLine("Weekday");
+    break;
+
+        case 6: case 7:
+
+    Console.WriteLine("Weekend");
+    break;
+
+    default:
+    Console.WriteLine("This number is wrong, please try again!");
+    break;
+
+}
